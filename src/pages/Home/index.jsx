712e9283d99b4688108b49f992bb49d988c5cfaa
duduike <eduardo.happ@yahoo.com>
2024-06-04 12:59:48 -0300

    feat: create html base and map users

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,47 @@
 import './style.css'
+import Trash from '../../assets/trash.svg'
 
 function Home() {
 
+  const users = [
+    {
+      id: '24235456545gf',
+      name: 'Eduardo',
+      age: 28,
+      email: 'eduardo@email.com'
+    },
+    {
+      id: '231423432',
+      name: 'Sabrina',
+      age: 25,
+      email: 'sabrina@email.com'
+    }
+  ]
+
   return (
-    
-    <div>
-      <h1>Olá React</h1>
+
+    <div className='container'>
+      <form action="">
+        <h1>Cadastro de Usuários</h1>
+        <input type="text" name='nome' />
+        <input type="number" name='idade' />
+        <input type="email" name='email' />
+        <button type='button'>Cadastrar</button>
+      </form>
+
+      {users.map(user => (
+        <div key={user.id}>
+          <div>
+            <p>Nome: {user.name}</p>
+            <p>Idade: {user.age}</p>
+            <p>Email: {user.email}</p>
+          </div>
+          <button>
+            <img src={Trash} width={30} alt="" />
+          </button>
+        </div>
+      ))}
+
     </div>
   )
 }
